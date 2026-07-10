3f56bddef96d782f0636cddffb95ba4c2072a210
saman karimi <sam.karimi727@gmail.com>
2026-07-11 02:03:06 +0330

    update server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}); 
+
+
+
 app.listen(PORT , () => {
   console.log(`Server is running on port ${PORT}`);
 });
