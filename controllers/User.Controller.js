365b2d7d532644d29f317f69529b9f3288839230
saman karimi <sam.karimi727@gmail.com>
2026-07-13 03:22:35 +0330

    update User.Controller.js

--- controllers/User.Controller.js
+++ controllers/User.Controller.js
@@ -2,10 +2,11 @@ import userModel from "../models/User.model.js";
 import  Jwt  from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
-  const { name, email, password, role } = req.body;
+  const { name, email, password } = req.body;
 
   try {
     console.log(name, email, password, role);
+    res.status(400).json({name, email, password})
   } catch (error) {
     console.log(`Register User Faild :  ${error.message}`);
     res.status(500).json({ error: error.message });
