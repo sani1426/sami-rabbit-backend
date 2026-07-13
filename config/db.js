476f353015b63ccead63665c0fca08b5dd84759a
saman karimi <sam.karimi727@gmail.com>
2026-07-14 01:24:58 +0330

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,15 @@
 import mongoose from 'mongoose'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const cached = mongoose || { conn: null, promise: null }
 
- const connectToDb = async (
-  MONGO_URI = process.env.MONGO_URI
-) => {
-  if (cached.conn) return cached.conn
-
-  if (!MONGO_URI) throw new Error('MONGODB_URI is missing')
-
-  cached.promise = cached.promise || mongoose.connect(MONGO_URI)
-
-  cached.conn = await cached.promise
-console.log(cached.conn);
-  return cached.conn
+ const connectToDb = async () =>{
+const MONGO_URI = process.env.MONGO_URI
+try {
+  await mongoose.connect(MONGO_URI)
+  console.log('Connected to MongoDB')
+} catch (error) {
+ console.error('Failed to connect to MongoDB', error)
+ process.exit(1)  // Exit the process if connection fails
+}
 
 }
 
