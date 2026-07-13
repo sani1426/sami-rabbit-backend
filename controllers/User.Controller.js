3b5a22f0f99bde444a5dad062a77f08f01cb7210
saman karimi <sam.karimi727@gmail.com>
2026-07-14 01:12:24 +0330

    update User.Controller.js

--- controllers/User.Controller.js
+++ controllers/User.Controller.js
@@ -5,7 +5,7 @@ export const registerUser = async (req, res) => {
   const { name, email, password, role } = req.body;
 
   try {
-    let user = userModel.findOne({ email });
+    let user =await userModel.findOne({ email });
     if (user) {
       return res.status(400).json({ error: "user Already exist" });
     }
