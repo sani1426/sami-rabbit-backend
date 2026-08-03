c994af3a499c9e8d6fb9bedc01b0159150d43086
saman karimi <sam.karimi727@gmail.com>
2026-08-04 03:01:45 +0330

    update server.js

--- server.js
+++ server.js
@@ -18,9 +18,23 @@ dotenv.config();
 const app = express();
 
 //  middlewares
-app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://samirabbit.vercel.app",
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    preflightContinue: false,
+  })
+);
 
 //  routes
 app.use("/api/users", userRouter);
